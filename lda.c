69e4e0e3351cd6b72c37050783817ac96f7f3157
MOBIUSLOOPFOUR <scratch.joint-0i@icloud.com>
2021-11-17 01:02:51 +0100

    Remove unneeded null checks

--- lda.c
+++ lda.c
@@ -1,62 +1,44 @@
 #include "defs.h"
 
-#define LDA_RETURN(x) setf(c); cyc-=x; return 0;
+#define LDA_RETURN(x)                                                          \
+  setf(c);                                                                     \
+  cyc -= x;                                                                    \
+  return 0;
 
 static setf(struct cpu *const c);
 
 lda_imm(c) struct cpu *c;
 {
-  NULLCHK(c);
   c->acc = fetchb(c);
   LDA_RETURN(2);
 }
 
 lda_zpg(c) struct cpu *c;
-{
-  NULLCHK(c);
-
-  LDA_RETURN(3);
-}
+{ LDA_RETURN(3); }
 
 lda_zpx(c) struct cpu *c;
-{
-  NULLCHK(c);
-  
-  LDA_RETURN(4);
-}
+{ LDA_RETURN(4); }
 
 lda_abs(c) struct cpu *c;
-{
-  NULLCHK(c);
-  
-  LDA_RETURN(4);
-}
+{ LDA_RETURN(4); }
 
 lda_abx(c) struct cpu *c;
 {
-  NULLCHK(c);
   cyc -= 4; /* TODO: check for page crossing */
-
   LDA_RETURN(0);
 }
 
 lda_aby(c) struct cpu *c;
 {
-  NULLCHK(c);
   cyc -= 4; /* TODO: check for page crossing */
   LDA_RETURN(0);
 }
 
 lda_idx(c) struct cpu *c;
-{
-  NULLCHK(c);
-
-  LDA_RETURN(6);
-}
+{ LDA_RETURN(6); }
 
 lda_idy(c) struct cpu *c;
 {
-  NULLCHK(c);
   cyc -= 5; /* TODO: check for page crossing */
   LDA_RETURN(0);
 }
@@ -64,7 +46,6 @@ lda_idy(c) struct cpu *c;
 static setf(c) struct cpu *const c;
 {
   NULLCHK(c);
-
   if (!c->acc) {
     c->stat.nf = 1;
   }
